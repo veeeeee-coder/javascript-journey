cc50340077bf231f426e43cd602c033288ad8ab8
veeeeee-coder <vijayeshwaridesu@gmail.com>
2026-02-06 19:56:35 +0530

    Add challenge-3 practice

---
+++ challenge-3.js
@@ -0,0 +1,45 @@
+//3a. Create the text "My name is:" as a string.
+//'My name is: '
+
+//3b. Create your name as a string (e.g., "Simon").
+//'VEEEE'
+
+//3c. Using concatenation, add strings from 3a and 3b to create: "My name is: ___".
+//'My name is: ' + 'VEEEE'
+
+//3d. At a restaurant, order 1 coffee ($5) and 1 bagel ($3). Using math, calculate total cost and create text: "Total cost: $".
+//'Total cost: $' + (5 + 3)
+
+//3e. Do the same as 3d using a template string and interpolation.
+//`Total cost: $${5 + 3}`
+
+//3f. Display the text from 3e using alert(...).
+//alert(`Total cost: $${5 + 3}`);
+
+//3g. Order 1 coffee ($5.99) and 1 bagel ($2.95). Calculate total using math and create text: "Total cost: $" (hint: calculate in cents).
+//'Total cost: $' + (599 + 295) / 100
+
+//3h. Do the same as 3g using a template string and interpolation.
+//`Total cost: $${(599 + 295) / 100}`
+
+//3i. Display the text from 3h in a popup.
+//alert(`Total cost: $${(599 + 295) / 100}`);
+
+//3j. Using a multi-line string, create text from 3h and add: "Thank you, come again!". Display both lines in a popup.
+//alert(`Total cost: $${(599+295)/100} 
+//Thank you,come again!`);
+
+//Challenge ExercisesSetup: In the Amazon project, update the cart to 2 basketballs ($20.95 each) and 2 t-shirts ($7.99 each) with $4.99 shipping.
+//3k. Using interpolation, create first line of text (use math): Items (4): $57.88.
+//`Items (${2 + 2}): $${(2 * 2095 + 2 * 799) / 100}`
+
+//3l. Create second line: Shipping & handling: $9.98 (use math).
+//`Shipping & handling: $${(499 + 499) / 100}`
+
+//3m. Create third line: Total before tax: $67.86 (use math).
+//`Total before tax: $${(2 * 2095 + 2 * 799 + 499 + 499) / 100}`
+
+//3n. Create fourth line: Estimated tax (10%): $6.79 (use math and Math.round(...)).
+//`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`
+
+
