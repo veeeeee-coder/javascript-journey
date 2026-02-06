84e4cb3004df9962351dc48b6f43ef6e24982f18
veeeeee-coder <vijayeshwaridesu@gmail.com>
2026-02-06 10:10:19 +0530

    challenge-1(practice)

---
+++ challenge-1.js
@@ -0,0 +1,37 @@
+//1a)Use the alert function to display the message "Good morning!" in a pop-up box.
+alert('Good morning!');
+
+//1b)Use the alert function to display the message(name) "veeeee" in a pop-up box.
+alert('veeeee');
+
+//1c)Using math,calculate 10+5 in console
+//>10+5
+//>15
+
+//1d)calculate 20-5
+//>20-5
+//>15
+
+//1e)calculate 2+2-5
+//>2+2-5
+//>-1
+
+//1f)use document.body.innerHTML=..; to display good morning on web page
+document.body.innerHTML = 'Good morning';
+
+//1g)use document.body.innerHTML=..; to display your name on web page
+document.body.innerHTML = 'veeeee';
+
+//Challenge exercises
+
+//1h)You order a T-shirt for $10, socks for $8, and dinner plates for $20.
+//Use JavaScript to calculate the total cost of your order.
+//10+8+20
+//>38
+
+//1i) Your bank account has $100, you spend $20 on lunch, $50 on dinner, and earn $200 from your job. Calculate how much money you have.
+//100-20-50+200
+//>230
+
+//1j) Use document.body.innerHTML = ..; to make the web page blank.
+document.body.innerHTML = '';
